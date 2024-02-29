ae177137640136d5ff53fb173692b428ea1fcd54
Robert <120193333+GrimReaper91@users.noreply.github.com>
2024-02-29 14:58:02 +0100

    Lektion 1

---
+++ server/models/cart.js
@@ -0,0 +1,9 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define("cart", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+  });
+};
