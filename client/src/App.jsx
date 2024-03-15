78c3de76bbe8a2723e9d9b3e864ac981696ae82e
Robert <120193333+GrimReaper91@users.noreply.github.com>
2024-03-15 10:33:21 +0100

    appbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,21 @@
 import { Link, Outlet } from "react-router-dom";
+import { Box, AppBar, Toolbar, Typography, Button } from "@mui/material";
 
 function App() {
   return (
     <>
-      <ul>
-        <li>
-          <Link to="/carts/new">Skapa inlägg</Link>
-        </li>
-        <li>
-          <Link to="/">Affär</Link>
-        </li>
-      </ul>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link to="/">Store</Link>
+            </Typography>
+            <Button color="inherit">
+              <Link to="/carts/new">Skapa inlägg</Link>
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <Outlet />
     </>
   );
